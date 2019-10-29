8ceeb263c48bbd2c4bf85b6195d345c64eb6a3d8
yosuke.yamauchi <yosuke.yamauchi0129@gmail.com>
2019-10-29 23:30:06 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
