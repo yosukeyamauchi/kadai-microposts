6f18c85b6f532c7117332f47d60d4d7257aed455
yosuke.yamauchi <yosuke.yamauchi0129@gmail.com>
2019-11-03 14:16:36 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,8 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @micropost = current_user.microposts.build
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
   end
 end
